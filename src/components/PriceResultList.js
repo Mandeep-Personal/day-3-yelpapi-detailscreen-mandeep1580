7532026377508c4e70fa7af16c666a8ac62c4de0
mandeep1580 <mandeep15@gmail.com>
2021-05-09 17:08:10 -0700

    added props and button

--- src/components/PriceResultList.js
+++ src/components/PriceResultList.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {StyleSheet, View, Text, FlatList } from 'react-native';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import PriceResultItem from './PriceResultItem';
 
 const PriceResultList = (props) => {
-
+    const showDetails = (detail) => {
+        props.navigation.navigate('DetailScreen', detail)
+      }
     return(
         <View>
             <Text style={styles.title}>{props.title}</Text>
@@ -11,8 +13,12 @@ const PriceResultList = (props) => {
             data={props.results}
             keyExtractor={(business) => business.id}
             renderItem={({ item }) => { 
-                return <PriceResultItem result={item}/>
+                return (
+                <TouchableOpacity onPress={() => showDetails(item)}>
+                <PriceResultItem result={item}/>
+                </TouchableOpacity>)
             }}
+
             horizontal={true}
             />
         </View>
